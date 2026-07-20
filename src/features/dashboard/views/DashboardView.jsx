c36e012f6475b4d4f2ac29491df362b46f7b401e
DapinCi <alfarizimuhammaddafin@gmail.com>
2026-07-20 23:16:01 +0700

    first setup n commit

---
+++ src/features/dashboard/views/DashboardView.jsx
@@ -0,0 +1,9 @@
+// ketika error maka tampilkan
+// ErrorState.jsx
+
+// ketika user belum login maka tampilkan
+// <HalamanLogin/>
+
+// ketika berhasil dirender maka tampilkan
+// <HeroDash/>
+// <BodyDash/>
